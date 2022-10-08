6a7262b38e325b772b33fa3a671bdd13a9de724d
Mohammed-Mshal <mohammedmshal02@gmail.com>
2022-10-08 18:47:54 +0300

    Update

--- src/Components/logo/Logo.js
+++ src/Components/logo/Logo.js
@@ -7,28 +7,28 @@ function Logo() {
       <motion.img
         initial={{ y: 40, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
-        transition={{delay:.5}}
+        transition={{ delay: 0.5 }}
         src="https://www.webstickersmuraux.com/fr/img/asfs360-jpg/folder/products-listado-merchanthover/autocollants-nike-logo.jpg"
         alt="nike "
-        className="w-75 logo"
+        className="w-75 h-auto logo"
         style={{ maxWidth: `150px` }}
       />
       <motion.img
         initial={{ y: 40, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
-        transition={{delay:.5}}
-        src="https://www.webstickersmuraux.com/fr/img/asfs360-jpg/folder/products-listado-merchanthover/autocollants-nike-logo.jpg"
+        transition={{ delay: 0.5 }}
+        src="https://1757140519.rsc.cdn77.org/blog/wp-content/uploads/2020/03/the-4th-logo-1024x862.jpg"
         alt="nike "
-        className="w-75 mx-3 logo"
+        className="w-75 h-auto mx-3 logo"
         style={{ maxWidth: `100px`, maxHeight: `100px` }}
       />
       <motion.img
         initial={{ y: 40, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
-        transition={{delay:.5}}
+        transition={{ delay: 0.5 }}
         src="https://www.logo.wine/a/logo/Converse_(shoe_company)/Converse_(shoe_company)-Icon-Logo.wine.svg"
         alt="nike "
-        className="w-75 logo"
+        className="w-75 h-auto logo"
         style={{ maxWidth: `150px` }}
       />
     </div>
